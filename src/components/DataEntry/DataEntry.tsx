bbcb1d0856d66f0d1d6833a35ee3cb9416d43ab3
Maksim Andreev <ma.andreev@corp.vk.com>
2022-07-23 12:35:53 +0300

    Add caret MVP

--- src/components/DataEntry/DataEntry.tsx
+++ src/components/DataEntry/DataEntry.tsx
@@ -1,4 +1,12 @@
-import { forwardRef, FormEvent } from 'react';
+import {
+  CSSProperties,
+  FocusEvent,
+  FormEvent,
+  forwardRef,
+  KeyboardEvent,
+  MouseEvent,
+  useState,
+} from 'react';
 
 import { PREFIX } from '../../constants';
 import { Command } from '../../types';
@@ -12,23 +20,52 @@ type Props = {
 
 export const DataEntry = forwardRef<HTMLInputElement, Props>(
   ({ onChange, onSubmit }, ref) => {
+    const [caretIndex, setCaretIndex] = useState(0);
+
+    const handleInputEvent = <T extends HTMLInputElement>(
+      event: MouseEvent<T> | FocusEvent<T> | KeyboardEvent<T>
+    ) => {
+      const target: HTMLInputElement = event.target as HTMLInputElement;
+
+      if (target.selectionStart === target.selectionEnd) {
+        setCaretIndex(target.selectionStart ?? 0);
+      }
+    };
+
     const handleSubmit = (event: FormEvent) => {
       event.preventDefault();
       onSubmit();
     };
 
+    const caretPosition = {
+      '--index': caretIndex,
+    } as CSSProperties;
+
     return (
       <form className="DataEntry" onSubmit={handleSubmit}>
-        <span>{PREFIX}</span>
-        <input
-          autoCorrect="off"
-          autoFocus
-          className="DataEntry__input"
-          ref={ref}
-          spellCheck={false}
-          type="text"
-          onChange={({ target }) => onChange(target.value)}
-        />
+        <div role="presentation">{PREFIX}</div>
+        <div className="DataEntry__interact">
+          <div
+            className="DataEntry__caret"
+            role="presentation"
+            style={caretPosition}
+          >
+            m
+          </div>
+          <input
+            autoCorrect="off"
+            autoFocus
+            className="DataEntry__input"
+            ref={ref}
+            spellCheck={false}
+            type="text"
+            onChange={({ target }) => onChange(target.value)}
+            onClick={handleInputEvent}
+            onContextMenu={handleInputEvent}
+            onFocus={handleInputEvent}
+            onKeyDown={handleInputEvent}
+          />
+        </div>
       </form>
     );
   }
